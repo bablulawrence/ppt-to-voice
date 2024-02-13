7308b87209db51eb829abbacd6639f7ce61c2b27
Bablu Lawrence <bablu.61052066@ltimindtree.com>
2024-02-13 14:12:43 +0530

    Fixed the slide ordering issue

--- src/app.py
+++ src/app.py
@@ -1,7 +1,8 @@
+import os
 import streamlit as st
 from PIL import Image
 from pathlib import Path
-import os
+from dotenv import load_dotenv
 
 # Load images
 def load_images(folder):
@@ -15,32 +16,49 @@ def load_audio(folder):
 
 # Set up the main structure of the app
 def main():
-    st.title('CogniDeck')
+    load_dotenv(override=True)
+    # Custom CSS to define your title size
+    st.markdown("""
+    <style>
+    .custom-title {
+        font-size:28px;
+        font-weight:bold;
+        color: darkblue; /* Blue color */
+        text-align: center; /* Center alignment */
+        margin-top: -20px; /* Move up the title */
+    }
+    </style>
+    """, unsafe_allow_html=True)
+
+    # Using the custom CSS class for the title
+    st.markdown('<div class="custom-title">CogniDeck</div>', unsafe_allow_html=True)
+    st.divider()
+    # st.title('CogniDeck')
     
     # Folder where images and audio files are stored
-    root_folder_path = Path("C:/Users/61052067/data/pptread")
-    input_folder_name = "life_sciences_regulatory_101_sample1"
-    process_folder_path = root_folder_path / input_folder_name
+    # root_folder_path = Path("C:/Users/61052067/data/pptread")
+    # input_folder_name = "life_sciences_regulatory_101_sample1"
+    folder_path = Path(os.getenv('PRESENTATION_FILE_FOLDER_PATH'))
 
     # Load images and audio files
-    images = load_images(process_folder_path)
-    audio_files = load_audio(process_folder_path)
+    images = load_images(folder_path)
+    audio_files = load_audio(folder_path)
     
     # Create a session state for the current slide
     if 'current_slide' not in st.session_state:
         st.session_state.current_slide = 0
     
     # Display the image
-    image = Image.open(os.path.join(process_folder_path, images[st.session_state.current_slide]))
+    image = Image.open(os.path.join(folder_path, images[st.session_state.current_slide]))
     st.image(image, caption=f'Slide {st.session_state.current_slide + 1}')
     
     # Display the audio player
-    audio_file = open(os.path.join(process_folder_path, audio_files[st.session_state.current_slide]), 'rb')
+    audio_file = open(os.path.join(folder_path, audio_files[st.session_state.current_slide]), 'rb')
     audio_bytes = audio_file.read()
     st.audio(audio_bytes, format='audio/mp3')
 
     # Navigation buttons
-    col1, col2 = st.columns(2)
+    col1, col2 = st.columns([10, 1])
     with col1:
         if st.button('Previous'):
             if st.session_state.current_slide > 0:
